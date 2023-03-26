1f504db777d15094d3f4c84ff2b671283e9193db
Ahmad Mukhtar <ahmedmukhtar1133@gmail.com>
2023-03-27 00:54:57 +0500

    Product name changes.

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -57,14 +57,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About UNHCR Handbookct',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide UNHCR Handbookct',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
